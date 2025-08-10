404d09db122ffdca72bc59dc5ee78ac0a09e5362
Augustine Joseph <augustinejosephj4@gmail.com>
2025-08-10 23:18:43 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Component } from '@angular/core';
+import { Districts } from './pages/districts/districts';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+
+
+ {
+  path: '',
+  component:Home
+
+},
+
+{
+    path:'districts',
+    component:Districts
+},
+{
+
+    path: 'contact-us',
+    component:Contact
+}
+
+];
---
+++ src/app/pages/districts/kasaragod/kasaragod.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kasaragod',
+  imports: [],
+  templateUrl: './kasaragod.html',
+  styleUrl: './kasaragod.css'
+})
+export class Kasaragod {
+
+}
